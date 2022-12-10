338363dbe8acde938a4abf6c9061fdfad49ddcf5
Victor Hugo <victorhugocassiano@hotmail.com>
2022-12-10 20:53:57 -0300

    Mudança de grid e botão de limpar

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
-// Cria um elemento div que será o container
 var container = document.getElementById('container')
 
+var elementos = "square16x16"
+let nQuadrados = 0
 
 
+ 
 /// Cria um elemento div com a classe grid
 var grid = document.createElement("div");
 grid.className = "grid";
 
-// Adiciona 256 elementos div com a classe square à grid
-for (var i = 0; i < 256; i++) {
-  var square = document.createElement("div");
-  square.className = "square";
 
-  // Adiciona um event listener de mouseover ao quadrado
-  square.addEventListener("mouseover", function() {
-    // Define a cor de fundo do quadrado como preto quando o mouse passar por cima dele
-    this.style.backgroundColor = "black";
-  });
+  function s64x64(){
+    removerGrid(nQuadrados)
+     nQuadrados = 4096
+    criarGrid(4096,"square64x64")
+  }
+  function s32x32(){
+    removerGrid(nQuadrados)
+    nQuadrados = 1024
+    criarGrid(1024,"square32x32")
+  }
+  function s16x16(){
+    removerGrid(nQuadrados)
+    nQuadrados = 256
+    criarGrid(256,"square16x16")
+  }
+//Cria a grid do tamanho escolhido com as funcoes acima
+    function criarGrid(nQuadrados,elementos){
+  for (var i = 0; i < nQuadrados; i++) {
+    var square = document.createElement("div");
+    square.className = elementos;
+    square.id = "square"
+  
+    square.addEventListener("mouseover", function() {
+      // Define a cor de fundo do quadrado como preto quando o mouse passar por cima dele
+      this.style.backgroundColor = "black";
+    });
 
-  grid.appendChild(square);
+    grid.appendChild(square);
+  }
 }
+
+ function removerGrid(nQuadrados){
+   removerQuadrado = document.querySelectorAll('#square')
+  for (let i = 0; i < nQuadrados; i++){
+    removerQuadrado[i].remove()
+  }
+ }
 
 container.appendChild(grid);
 function Limpar(){
-  document.querySelectorAll(".square").style.backgroundColor = "white";
+   // Volta todos os quadrados da grid para branco
+   var limparQuadrado = document.querySelectorAll('#square');
+   for (var i = 0; i < limparQuadrado.length; i++) {
+    limparQuadrado[i].style.backgroundColor = "white";
+   }
 }
