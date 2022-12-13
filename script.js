db88615daee96684c279e93802e9bfa9c6e66d2b
Victor Hugo <victorhugocassiano@hotmail.com>
2022-12-13 02:00:37 -0300

    design dos botões
    Slide range button pro tamanho da grid
    e design do botão de limpar

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var container = document.getElementById('container')
 
 var elementos = "square16x16"
@@ -25,6 +26,7 @@ grid.className = "grid";
     nQuadrados = 256
     criarGrid(256,"square16x16")
   }
+  s16x16()
 //Cria a grid do tamanho escolhido com as funcoes acima
     function criarGrid(nQuadrados,elementos){
   for (var i = 0; i < nQuadrados; i++) {
@@ -55,4 +57,33 @@ function Limpar(){
    for (var i = 0; i < limparQuadrado.length; i++) {
     limparQuadrado[i].style.backgroundColor = "white";
    }
+}
+
+var slider = document.getElementById("gridSlide");
+var output = document.getElementById("valordaGrid");
+output.innerHTML = "16x16";
+
+slider.oninput = function() {
+  outputGrid = this.value;
+  let tamanhoGrid = this.value
+  if(tamanhoGrid==1){
+    s16x16()
+  }
+  else if(tamanhoGrid==2){
+    s32x32()
+  }
+  else if(tamanhoGrid==3){
+    s64x64()
+  }
+  
+  if(outputGrid==1){
+    outputGrid="16x16"
+  }
+  else if(outputGrid==2){
+    outputGrid="32x32"
+  }
+  else if(outputGrid==3){
+    outputGrid="64x64"
+  }
+  output.innerHTML = outputGrid;
 }
